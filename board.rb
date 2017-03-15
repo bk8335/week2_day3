65e620582821bd91099c77d2e32f170c929ca37a
Benjamin <benjamin.kinnard1@gmail.com>
2017-03-15 12:11:25 +0000

    Board can now handle pieces

--- board.rb
+++ board.rb
@@ -2,8 +2,16 @@ class Board
   
   attr_reader :board_size
 
-  def initialize()
+  def initialize(pieces)
+    @pieces = pieces
     @board_size = 100
   end
+
+  def position(piece_name)
+    for each_piece in @pieces
+        return each_piece.position if each_piece.name == piece_name
+     end
+  end
+
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,14 +1,26 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../board')
+require_relative('../piece')
 
 class TestBoard < MiniTest::Test
 
   def setup()
-    @board1 = Board.new()
+    @piece1 = Piece.new("Ben")
+    @piece2 = Piece.new("Andrew")
+    @piece3 = Piece.new("Sandy")
+
+    pieces = [@piece1, @piece2, @piece3]
+
+    @board1 = Board.new(pieces)
   end
 
   def test_board_size
     assert_equal(100, @board1.board_size)
+  end
+
+  def test_piece_position
+    assert_equal(0, @piece1.position)
+    assert_equal(0, @board1.position("Ben"))
   end
 end
